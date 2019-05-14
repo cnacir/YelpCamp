c2a8a3dee9f9512e4de8d39fe5110aa119bf78a6
cnacir <cknacir@hotmail.com>
2019-05-14 17:28:39 -0400

    connected mongoose to mongodb, edited view logic, and fixed spacing of elements on campgrounds page

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const express = require('express'),
 			bodyParser = require('body-parser'),
  			mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+
+// Campground.create(
+// 	{
+// 		name: "Salmon Creek",
+// 		image: "https://www.photosforclass.com/download/flickr-7626464792"
+// 	}, (err, campground) => {
+// 		if(err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log(`NEWLY CREATED CAMPGROUND: ${campground}`)
+// 		};
+// 	});
+
+
 let campgrounds = [
 	{name: "Salmon Creek", image: "https://www.photosforclass.com/download/flickr-7626464792"},
 	{name: "Granite Hill", image: "https://www.photosforclass.com/download/flickr-321487195"},
@@ -12,16 +39,21 @@ let campgrounds = [
 	{name: "Mountain Goat Rest", image: "https://farm4.staticflickr.com/3130/2770459706_3aed20703e.jpg"}
 ]
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
 	res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-	res.render("campgrounds", {campgrounds: campgrounds});
+	Campground.find({}, (err, allCampgrounds) => {
+		if(err) {
+			console.log(err)
+		} else {
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		};
+	});
+
 });
 
 app.post("/campgrounds", (req, res) => {
@@ -29,8 +61,13 @@ app.post("/campgrounds", (req, res) => {
 	const image = req.body.image;
 
 	let newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+	Campground.create(newCampground, (err, newlyCreated) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		};
+	});
 });
 
 app.get("/campgrounds/new", (req, res) => {
